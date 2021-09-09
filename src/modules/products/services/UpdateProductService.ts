fe63496faa4b4522222c3dce047f195e6155e2dc
Danillao <joufilho@gmail.com>
2021-09-09 14:27:40 -0300

    subindo arquivos iniciais da api

---
+++ src/modules/products/services/UpdateProductService.ts
@@ -0,0 +1,39 @@
+import AppError from '@shared/errors/AppError';
+import { getCustomRepository } from 'typeorm';
+import Product from '../typeorm/entities/Product';
+import  { ProductRepository }  from '../typeorm/repositories/ProductsRepository';
+
+
+interface IRequest {
+  id: string;
+  name: string;
+  price: number;
+  quantity: number;
+}
+
+class UpdateProductService {
+  public async execute({ id, name, price, quantity }: IRequest): Promise<Product | undefined> {
+    const productsRepository = getCustomRepository(ProductRepository);
+    const productExists = await productsRepository.findByName(name);
+    const product = await productsRepository.findOne(id);
+
+    if(!product) {
+      throw new AppError('produto não encontrado');
+    }
+
+    if(productExists) {
+      throw new AppError('Ja existe um produto com esse nome');
+    }
+
+    product.name = name;
+    product.price = price;
+    product.quantity = quantity;
+
+    await productsRepository.save(product);
+
+    return product;
+  }
+}
+
+
+export default UpdateProductService;
